4d24baad5360d7d5ae84376487afb73374c27a7a
gianlucagalota <gianluca.galota@gmail.com>
2024-01-24 20:38:33 +0100

    implemented auth router/controller/schema/validation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from './routes/userRoutes.ts';
+import authRoutes from './routes/authRoutes.ts';
 
 dotenv.config();
 
@@ -15,11 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-
-app.get('/api/test', async (req: Request, res: Response) => {
-  res.json({ message: 'hello from express endpoint!' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
